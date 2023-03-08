32933b6f10b2a16602ddb0d66365d67080e1df49
Ava4Everr <59756295+Ava4Everr@users.noreply.github.com>
2023-03-08 11:30:42 -0500

    Update 8.2.8 Tic Tac Toe Methods (TicTacToe).java

--- Unit 8 - 2D Array/8.2.8 Tic Tac Toe Methods (TicTacToe).java
+++ Unit 8 - 2D Array/8.2.8 Tic Tac Toe Methods (TicTacToe).java
@@ -21,7 +21,18 @@ public class TicTacToe
    /*This method prints out the board array on to the console
    */
    public void printBoard()
-    {   
+    {
+        /*
+        System.out.println("\n  0 1 2");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        */
+        
         System.out.println("  0 1 2");
         int row = 0;
         for(String[] array : board)
@@ -40,12 +51,13 @@ public class TicTacToe
    //This method returns true if space row, col is a valid space
    public boolean pickLocation(int row, int col)
    {
+        /*
         if (board[row][col] == "-") {
            return true;
-        } /*else {
-            return false;  
-        }*/
+        }
         return false;
+        */
+        return board[row][col] == "-";
    }
    
    //This method places an X or O at location row,col based on the int turn
@@ -62,7 +74,7 @@ public class TicTacToe
    //This method returns a boolean that returns true if a row has three X or O's in a row
    public boolean checkRow()
    {
-        for (int i = 0; i < board.length; i++){
+        for (int i = 0; i < board.length; i++) {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != "-") {
                 return true;
             }
@@ -73,7 +85,7 @@ public class TicTacToe
     //This method returns a boolean that returns true if a col has three X or O's
    public boolean checkCol()
    {
-       for (int j = 0; j < board.length; j++){
+       for (int j = 0; j < board.length; j++) {
             if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != "-") {
                 return true;
             }
@@ -93,10 +105,10 @@ public class TicTacToe
    //This method returns a boolean that checks if someone has won the game
    public boolean checkWin()
    {
-       if (checkRow() || checkCol() || checkDiag()) {
-           return true;
-       }
-       return false;
+        if (checkRow() || checkCol() || checkDiag()) {
+            return true;
+        }
+        return false;
    }
 
 }
